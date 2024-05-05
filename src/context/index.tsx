18d4d73526f1fa75ad43ae39003445ed59b01c89
Cherik <Pourcheriki@gmail.com>
2024-05-05 15:32:36 +0330

    add wallet context

---
+++ src/context/index.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import React, { ReactNode } from "react";
+
+import { createWeb3Modal } from "@web3modal/wagmi/react";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { State, WagmiProvider } from "wagmi";
+import { projectId, wagmiConfig } from "@/cofig/wagmi";
+
+// Setup queryClient
+const queryClient = new QueryClient();
+
+if (!projectId) throw new Error("Project ID is not defined");
+
+// Create modal
+createWeb3Modal({
+  wagmiConfig: wagmiConfig,
+  projectId,
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+  enableOnramp: true, // Optional - false as default
+});
+
+export default function Web3ModalProvider({
+  children,
+  initialState,
+}: {
+  children: ReactNode;
+  initialState?: State;
+}) {
+  return (
+    <WagmiProvider config={wagmiConfig} initialState={initialState}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  );
+}
