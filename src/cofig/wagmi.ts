2bccd1ce92831786f638933d46e9a21d83994216
Cherik <Pourcheriki@gmail.com>
2024-05-05 15:32:17 +0330

    add wagmi configs

---
+++ src/cofig/wagmi.ts
@@ -0,0 +1,29 @@
+import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
+
+import { cookieStorage, createStorage } from "wagmi";
+import { mainnet, sepolia } from "wagmi/chains";
+
+// Get projectId at https://cloud.walletconnect.com
+export const projectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_ID;
+
+if (!projectId) throw new Error("Project ID is not defined");
+
+const metadata = {
+  name: "DeVouch",
+  description: "DeVouch Example",
+  url: "https://DeVouch.com", // origin must match your domain & subdomain
+  icons: ["https://avatars.githubusercontent.com/u/37784886"],
+};
+
+// Create wagmiConfig
+const chains = [mainnet, sepolia] as const;
+export const wagmiConfig = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata,
+  ssr: true,
+  storage: createStorage({
+    storage: cookieStorage,
+  }),
+  //   ...wagmiOptions, // Optional - Override createConfig parameters
+});
