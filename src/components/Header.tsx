1b1bd7eb0005985612a00661fc4dfd803e51d876
Cherik <Pourcheriki@gmail.com>
2024-05-07 13:11:06 +0330

    add logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,10 @@
+import Image from "next/image";
 import React from "react";
 
 export const Header = () => {
-  return <div className="container mx-auto py-10">Header</div>;
+  return (
+    <div className="container mx-auto py-10 flex">
+      <Image src="/images/logo.svg" alt="logo" width={165} height={30} />
+    </div>
+  );
 };
