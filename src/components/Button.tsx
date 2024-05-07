62e2b13880ac0673e4abfad08bb2b542619e0ff5
Cherik <Pourcheriki@gmail.com>
2024-05-07 15:21:51 +0330

    change bg-color blue

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,15 +2,12 @@ import React from "react";
 
 export const Button = () => {
   return (
-    <div className="group relative m-2">
+    <div className="group relative m-2  text-white">
       <div
         id="shadow"
         className="absolute w-full h-full bg-black z-0  bottom-0 animate-move-bounce-leave transform group-hover:animate-move-bounce-enter"
       />
-      <button
-        type="button"
-        className="bg-red-500 text-white z-1 relative py-2 px-4 "
-      >
+      <button type="button" className="bg-c-blue z-1 relative py-2 px-4 ">
         Click Me!
       </button>
     </div>
