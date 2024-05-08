6fa82b513f5f716c15bc9aee8aec9e5fd0ccf060
Cherik <Pourcheriki@gmail.com>
2024-05-08 10:25:21 +0330

    accept children for button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+import { Children, type FC } from "react";
 
-export const Button = () => {
+interface ButtonProps {
+  children: React.ReactNode;
+}
+
+export const Button: FC<ButtonProps> = ({ children }) => {
   return (
     <div className="group relative m-4  text-white h-10">
       <div
@@ -11,7 +15,7 @@ export const Button = () => {
         type="button"
         className="bg-c-blue-200 z-1 relative py-2 px-4 group-hover:animate-color-bounce-enter"
       >
-        Click Me!
+        {children}
       </button>
     </div>
   );
