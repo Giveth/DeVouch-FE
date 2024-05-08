c93f70a4ed6dc854813991bc386b12a88a8c54c7
Cherik <Pourcheriki@gmail.com>
2024-05-08 10:25:33 +0330

    show wallet address

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Header } from "@/components/Header";
+import { Header } from "@/components/Header/Header";
 import Image from "next/image";
 
 export default function Home() {
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,16 @@
+"use client";
+
 import Image from "next/image";
 import React from "react";
-import { Button } from "./Button";
+import { Button } from "../Button";
+import { useAccount } from "wagmi";
 
 export const Header = () => {
+  const { address, isConnected, isConnecting } = useAccount();
   return (
     <div className="container mx-auto py-10 flex justify-between items-center">
       <Image src="/images/logo.svg" alt="logo" width={165} height={30} />
-      <Button />
+      {address ? <Button>{address}</Button> : <Button>Connect Wallet</Button>}
     </div>
   );
 };
