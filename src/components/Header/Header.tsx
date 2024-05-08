1e04aae594e721aaeb23d467a45aed4bfcab0adc
Cherik <Pourcheriki@gmail.com>
2024-05-08 10:44:03 +0330

    show summarized address

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,13 +4,18 @@ import Image from "next/image";
 import React from "react";
 import { Button } from "../Button";
 import { useAccount } from "wagmi";
+import { summarizeAddress } from "@/helpers/wallet";
 
 export const Header = () => {
   const { address, isConnected, isConnecting } = useAccount();
   return (
     <div className="container mx-auto py-10 flex justify-between items-center">
       <Image src="/images/logo.svg" alt="logo" width={165} height={30} />
-      {address ? <Button>{address}</Button> : <Button>Connect Wallet</Button>}
+      {address ? (
+        <Button>{summarizeAddress(address)}</Button>
+      ) : (
+        <Button>Connect Wallet</Button>
+      )}
     </div>
   );
 };
