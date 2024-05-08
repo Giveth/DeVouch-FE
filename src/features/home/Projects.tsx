b1b3af712798f348d462cd5db9904319a5961289
Cherik <Pourcheriki@gmail.com>
2024-05-08 21:10:51 +0330

    add projects section

--- src/features/home/Projects.tsx
+++ src/features/home/Projects.tsx
@@ -4,9 +4,11 @@ import { useState } from 'react';
 export const Projects = () => {
 	const [sort, setSort] = useState();
 	return (
-		<div className='flex'>
+		<div className='container mx-auto'>
 			<div className='flex'>
-				<p className='text-gray-50'>Sort By</p>
+				<div className='flex'>
+					<p className='text-gray-400'>Sort By</p>
+				</div>
 			</div>
 		</div>
 	);
