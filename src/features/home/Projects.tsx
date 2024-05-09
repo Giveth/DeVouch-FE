1ad9477ee62c1fb4fab5309bad9414211b8157d1
Cherik <Pourcheriki@gmail.com>
2024-05-09 10:59:08 +0330

    add sort project

--- src/features/home/Projects.tsx
+++ src/features/home/Projects.tsx
@@ -1,13 +1,55 @@
 'use client';
 import { useState } from 'react';
+import { Select, type IOption } from '@/components/Select/Select';
+
+enum EProjectSort {
+	NEWEST = 'newest',
+	OLDEST = 'oldest',
+	HIGHEST_VOUCH_COUNT = 'highest_count',
+	LOWEST_VOUCH_COUNT = 'lowest_count',
+	HIGHEST_FLAG = 'highest_flag',
+	LOWEST_FLAG = 'lowest_flag',
+}
+
+const sortOptions: IOption[] = [
+	{
+		key: EProjectSort.NEWEST,
+		value: 'Newest',
+	},
+	{
+		key: EProjectSort.OLDEST,
+		value: 'Oldest',
+	},
+	{
+		key: EProjectSort.HIGHEST_VOUCH_COUNT,
+		value: 'Highest Vouch Count',
+	},
+	{
+		key: EProjectSort.LOWEST_VOUCH_COUNT,
+		value: 'Lowest Vouch Count',
+	},
+	{
+		key: EProjectSort.HIGHEST_FLAG,
+		value: 'Highest Flag',
+	},
+	{
+		key: EProjectSort.LOWEST_FLAG,
+		value: 'Lowest Flag',
+	},
+];
 
 export const Projects = () => {
-	const [sort, setSort] = useState();
+	const [sort, setSort] = useState(sortOptions[0]);
 	return (
 		<div className='container mx-auto'>
 			<div className='flex'>
-				<div className='flex'>
+				<div className='flex gap-4 items-center'>
 					<p className='text-gray-400'>Sort By</p>
+					<Select
+						options={sortOptions}
+						value={sort}
+						setValue={setSort}
+					/>
 				</div>
 			</div>
 		</div>
