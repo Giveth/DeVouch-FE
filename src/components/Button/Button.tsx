405c8574ac8955eef46756298687161cd31969a5
Cherik <Pourcheriki@gmail.com>
2024-05-13 14:23:42 +0330

    add button type to button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,21 @@
 import { type HTMLAttributes, type FC } from 'react';
+import { ButtonType, buttonTypeToStyle } from './common';
 
 interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
 	children: React.ReactNode;
+	buttonType?: ButtonType;
 }
 
-export const Button: FC<ButtonProps> = ({ children, ...props }) => {
+export const Button: FC<ButtonProps> = ({
+	children,
+	buttonType = ButtonType.FILLED_BLUE,
+	...props
+}) => {
 	return (
-		<div className='group/button relative m-4  text-white h-10'>
+		<div
+			className='group/button relative m-4  text-white h-10'
+			style={buttonTypeToStyle[buttonType] as any}
+		>
 			<div
 				id='shadow'
 				className='absolute w-full h-full bg-c-blue-200 z-0  bottom-0 animate-move-bounce-leave transform group-hover/button:animate-move-bounce-enter'
