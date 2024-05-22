6f4a16be6e8feef004b018ace1e5a73c925a58a3
Cherik <Pourcheriki@gmail.com>
2024-05-22 14:28:25 +0330

    add general filter

--- src/components/FilterMenu/FilterMenu.tsx
+++ src/components/FilterMenu/FilterMenu.tsx
@@ -1,39 +1,67 @@
-import {
-	type Dispatch,
-	type FC,
-	type HTMLAttributes,
-	type SetStateAction,
-} from 'react';
+import { type FC, type HTMLAttributes } from 'react';
 import Dropdown from '../Dropdown/Dropdown';
-import { IOption } from '../Select/Select';
 
 interface FilterMenuProps {
-	options: IOption[];
-	setValue: Dispatch<SetStateAction<IOption>>;
-	value: IOption;
+	options: IOptions;
+	setValue: (value: { key: string; values: string[] }) => void;
+	value: { [key: string]: string[] };
 	className?: HTMLAttributes<HTMLDivElement>['className'];
+	showChevron?: boolean;
+	stickToRight?: boolean;
 }
 
-export const FilterMenu: FC<FilterMenuProps> = ({
+interface IOptions {
+	[key: string]: string[];
+}
+
+const FilterMenu: FC<FilterMenuProps> = ({
 	options,
 	setValue,
 	value,
 	className,
+	showChevron = true,
+	stickToRight = false,
 }) => {
+	const handleCheckboxChange = (key: string, option: string) => {
+		const currentValues = value[key] || [];
+		const newValues = currentValues.includes(option)
+			? currentValues.filter(val => val !== option)
+			: [...currentValues, option];
+		setValue({ key, values: newValues });
+	};
+
+	const selectedCount = Object.values(value).reduce(
+		(acc, selectedOptions) => acc + selectedOptions.length,
+		0,
+	);
+
 	return (
 		<Dropdown
 			className={className}
-			options={options.map(option => (
-				<div
-					key={option.key}
-					// option={option}
-					// selected={option.key === value.key}
-					// setValue={setValue}
-				/>
+			options={Object.entries(options).map(([key, optionList]) => (
+				<div key={key}>
+					<div>{key}</div>
+					{optionList.map(option => (
+						<div
+							key={option}
+							className='flex items-center gap-2'
+							onClick={() => handleCheckboxChange(key, option)}
+						>
+							<input
+								type='checkbox'
+								checked={(value[key] || []).includes(option)}
+								readOnly
+							/>
+							<p>{option}</p>
+						</div>
+					))}
+				</div>
 			))}
-			label={value.value}
-			sameWidth
-			showChevron
+			label={`Filter (${selectedCount})`}
+			showChevron={showChevron}
+			stickToRight={stickToRight}
 		/>
 	);
 };
+
+export default FilterMenu;
