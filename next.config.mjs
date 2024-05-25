e84b00e3433fb6ae0670ac16fd09df7a525b0c85
Cherik <Pourcheriki@gmail.com>
2024-05-25 22:41:03 +0330

    add hosts

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,19 @@ const nextConfig = {
 		config.externals.push('pino-pretty', 'lokijs', 'encoding');
 		return config;
 	},
+
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'giveth.mypinata.cloud',
+			},
+			{
+				protocol: 'https',
+				hostname: 'static.tgbwidget.com',
+			},
+		],
+	},
 };
 
 export default nextConfig;
