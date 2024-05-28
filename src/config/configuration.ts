3830174b1ac0a9bb784e56e7566fc684e1e2ddcc
Cherik <Pourcheriki@gmail.com>
2024-05-28 12:15:56 +0330

    add configuration

---
+++ src/config/configuration.ts
@@ -0,0 +1,11 @@
+import development from './development';
+import production from './production';
+
+export const isProduction = process.env.NEXT_PUBLIC_ENV === 'production';
+const envConfig = isProduction ? production : development;
+
+const config = {
+	...envConfig,
+};
+
+export default config;
