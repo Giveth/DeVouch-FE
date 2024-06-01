d714f20f67d35b789d4b2a41f1614a5ddecb12c2
Cherik <Pourcheriki@gmail.com>
2024-06-01 17:35:23 +0330

    pass queryKey to ProjectCard

--- src/features/home/Projects.tsx
+++ src/features/home/Projects.tsx
@@ -115,7 +115,11 @@ export const Projects = () => {
 			<h2 className='text-2xl font-bold'>Explore Projects </h2>
 			<div className='grid grid-col-1 lg:grid-cols-2 gap-8 mb-8'>
 				{projects.map(project => (
-					<ProjectCard key={project.id} project={project} />
+					<ProjectCard
+						key={project.id}
+						project={project}
+						queryKey={['projects', filterValues, sort.key]}
+					/>
 				))}
 			</div>
 			{isLoading && (
