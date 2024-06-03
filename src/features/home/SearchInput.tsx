80026a6263360e6dc81c89d87e839d1de9351044
Cherik <Pourcheriki@gmail.com>
2024-06-03 19:44:25 +0330

    update search input styles

--- src/features/home/SearchInput.tsx
+++ src/features/home/SearchInput.tsx
@@ -9,7 +9,7 @@ export const SearchInput: FC<SearchInputProps> = ({ setTerm }) => {
 	return (
 		<input
 			placeholder='Search for projects'
-			className={`relative inline-block py-2 px-2 border border-white ${value ? 'flex-1 !border-black' : ''}  mb-2 outline-none focus:border-black focus:flex-1 transition-all`}
+			className={`relative inline-block py-2 px-2 border border-gray-300 hover:border-black ${value ? 'flex-1 !border-black' : ''}  mb-2 outline-none focus:border-black focus:flex-1 transition-all`}
 			value={value}
 			onChange={e => setValue(e.target.value)}
 			onKeyDown={e => {
