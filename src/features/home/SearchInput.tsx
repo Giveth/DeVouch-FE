9d9d40806f8eaf2866aded4d80a9111c812a3611
Cherik <Pourcheriki@gmail.com>
2024-06-04 13:08:36 +0330

    add right padding

--- src/features/home/SearchInput.tsx
+++ src/features/home/SearchInput.tsx
@@ -11,7 +11,7 @@ export const SearchInput: FC<SearchInputProps> = ({ setTerm }) => {
 		<>
 			<input
 				placeholder='Search for projects'
-				className={`relative inline-block py-2 px-2 border border-gray-300 hover:border-black ${value ? 'flex-1 !border-black' : ''}  mb-2 outline-none focus:border-black focus:flex-1 transition-all`}
+				className={`relative inline-block py-2 px-2 border border-gray-300 hover:border-black ${value ? 'flex-1 !border-black pr-16' : ''}  mb-2 outline-none focus:border-black focus:flex-1 focus:pr-16 transition-all`}
 				value={value}
 				onChange={e => setValue(e.target.value)}
 				onKeyDown={e => {
