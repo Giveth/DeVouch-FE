ad641fa074e7122f7daa4c4a9588990a6d3cd6d8
Cherik <Pourcheriki@gmail.com>
2024-06-04 09:58:58 +0330

    add icons

--- src/features/home/SearchInput.tsx
+++ src/features/home/SearchInput.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { type FC, useState } from 'react';
 
 interface SearchInputProps {
@@ -7,16 +8,54 @@ interface SearchInputProps {
 export const SearchInput: FC<SearchInputProps> = ({ setTerm }) => {
 	const [value, setValue] = useState<string>();
 	return (
-		<input
-			placeholder='Search for projects'
-			className={`relative inline-block py-2 px-2 border border-gray-300 hover:border-black ${value ? 'flex-1 !border-black' : ''}  mb-2 outline-none focus:border-black focus:flex-1 transition-all`}
-			value={value}
-			onChange={e => setValue(e.target.value)}
-			onKeyDown={e => {
-				if (e.key === 'Enter') {
-					value && setTerm(value);
-				}
-			}}
-		></input>
+		<>
+			<input
+				placeholder='Search for projects'
+				className={`relative inline-block py-2 px-2 border border-gray-300 hover:border-black ${value ? 'flex-1 !border-black' : ''}  mb-2 outline-none focus:border-black focus:flex-1 transition-all`}
+				value={value}
+				onChange={e => setValue(e.target.value)}
+				onKeyDown={e => {
+					if (e.key === 'Enter') {
+						value && setTerm(value);
+					}
+				}}
+			/>
+			<div className='relative'>
+				{value ? (
+					<div className='absolute -top-14 right-4 md:top-3 md:-left-20 cursor-pointer flex z-auto gap-3'>
+						<Image
+							src='/images/icons/right-arrow.svg'
+							width={20}
+							height={20}
+							alt='search'
+							className=''
+							onClick={() => {
+								value && setTerm(value);
+							}}
+						/>
+						<Image
+							src='/images/icons/x.svg'
+							width={20}
+							height={20}
+							alt='clear'
+							className=''
+							onClick={() => {
+								setValue('');
+								setTerm(''); // Clear the term as well
+							}}
+						/>
+					</div>
+				) : (
+					<div className='absolute -top-14 right-4  md:top-3 md:-left-12 cursor-pointer flex z-auto'>
+						<Image
+							src='/images/icons/search.svg'
+							width={20}
+							height={20}
+							alt='search'
+						/>
+					</div>
+				)}
+			</div>
+		</>
 	);
 };
