9ed902c7c9580bcfbe09fb032e4fe661d0234675
Cherik <Pourcheriki@gmail.com>
2024-06-06 00:13:28 +0330

    update routes

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -2,4 +2,7 @@ export enum ROUTES {
 	HOME = '/',
 	MY_ATTESTATIONS = '/profile/my-attestations',
 	SUPPORT = '/support',
+	PROJECTS = '/projects',
+	PROJECT = '/project',
+	PROFILE = '/profile',
 }
