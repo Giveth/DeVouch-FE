86f10e8b36f901f0b172af243cce46628c574013
Cherik <Pourcheriki@gmail.com>
2024-06-05 19:11:56 +0330

    add source badge icon

---
+++ src/components/SourceBadge.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+import { type FC, type HTMLAttributes } from 'react';
+import config from '@/config/configuration';
+
+interface SourceBadgeProps {
+	source: string;
+	className?: HTMLAttributes<HTMLDivElement>['className'];
+}
+
+export const SourceBadge: FC<SourceBadgeProps> = ({ source, className }) => {
+	console.log('source', source);
+	return (
+		<div
+			className={`absolute flex gap-1 bg-white py-1 px-2 z-auto ${className}`}
+		>
+			<span className='text-gray-300 font-light'>From</span>
+			<span className='text-black'>
+				{
+					config.SOURCE_PLATFORMS.find(
+						i => i.value.toLowerCase() === source.toLowerCase(),
+					)?.key
+				}
+			</span>
+			<Image
+				src={`/images/sources/${source}.svg`}
+				alt={source}
+				width={18}
+				height={18}
+			/>
+		</div>
+	);
+};
