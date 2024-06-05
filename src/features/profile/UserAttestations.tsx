d3321da3d3a85ab176fc5ab0e36e9b6d3edd17ed
Cherik <Pourcheriki@gmail.com>
2024-06-05 15:18:18 +0330

    show attests

--- src/features/profile/UserAttestations.tsx
+++ src/features/profile/UserAttestations.tsx
@@ -112,98 +112,104 @@ export const UserAttestations = ({
 						/>
 					</div>
 				) : (
-					<div className='grid grid-cols-6 items-center'>
-						{headers.map((header, id) => (
-							<div key={id}>{header.label}</div>
-						))}
-						{data?.attestations.map((attest, id) => (
-							<div key={id} className='contents'>
-								<div className='max-w-[220px] px-4 py-6 align-top text-gray-800'>
-									{attest.project.title}
-								</div>
-								<div>
-									{new Date(
-										attest.attestTimestamp,
-									).toLocaleDateString('en-US', {
-										day: 'numeric',
-										month: 'long',
-										year: 'numeric',
-									})}
-								</div>
-								<div className='bg-[#f7f7f9] px-2 py-1'>
-									{
-										attest.attestorOrganisation.organisation
-											.name
-									}
-								</div>
-								<div className='relative text-center w-[106px] px-4 py-6 align-top text-gray-800 z-50'>
-									{attest.comment ? (
-										<Tooltip
-											message={attest.comment}
-											direction='right'
-										>
+					<div className='overflow-x-auto'>
+						<div className='grid grid-cols-6 items-center min-w-[900px] text-left relative'>
+							{headers.map((header, id) => (
+								<div key={id}>{header.label}</div>
+							))}
+							{data?.attestations.map((attest, id) => (
+								<React.Fragment key={id}>
+									<div className='col-span-6 border-b'></div>{' '}
+									{/* Divider line */}
+									<div className='max-w-[220px] px-4 py-6 align-top text-gray-800'>
+										{attest.project.title}
+									</div>
+									<div>
+										{new Date(
+											attest.attestTimestamp,
+										).toLocaleDateString('en-US', {
+											day: 'numeric',
+											month: 'long',
+											year: 'numeric',
+										})}
+									</div>
+									<div className='bg-[#f7f7f9] px-2 py-1'>
+										{
+											attest.attestorOrganisation
+												.organisation.name
+										}
+									</div>
+									<div className='relative text-center w-[106px] px-4 py-6 align-top text-gray-800 z-50'>
+										{attest.comment ? (
+											<Tooltip
+												message={attest.comment}
+												direction='right'
+											>
+												<Image
+													src={
+														'/images/icons/msg.svg'
+													}
+													alt={'comment'}
+													width={18}
+													height={18}
+													className='cursor-pointer mx-auto'
+												/>
+											</Tooltip>
+										) : (
+											<b>-</b>
+										)}
+									</div>
+									<div>
+										{attest.vouch ? (
+											<span className='flex gap-2 items-center'>
+												<Image
+													src={
+														'/images/icons/vouched.svg'
+													}
+													alt={'vouched'}
+													width={24}
+													height={24}
+												/>
+												Vouched
+											</span>
+										) : (
+											<span className='gap-4 flex items-center'>
+												<Image
+													src={
+														'/images/icons/red-flag.svg'
+													}
+													alt={'red-flag'}
+													width={18}
+													height={18}
+												/>
+												Flagged
+											</span>
+										)}
+									</div>
+									<div className='flex flex-row px-4 py-6 align-top text-gray-800'>
+										<button className='flex flex-row mr-2 border border-gray text-black font-bold px-4 py-2 gap-2 items-center'>
+											Edit{' '}
 											<Image
-												src={'/images/icons/msg.svg'}
-												alt={'comment'}
-												width={18}
-												height={18}
-												className='cursor-pointer mx-auto'
+												src={'/images/icons/edit.svg'}
+												alt={'edit'}
+												width={16}
+												height={16}
 											/>
-										</Tooltip>
-									) : (
-										<b>-</b>
-									)}
-								</div>
-								<div>
-									{attest.vouch ? (
-										<span className='flex gap-2 items-center'>
+										</button>
+										<button className='mr-2 border border-gray text-black font-bold px-4 py-2'>
 											<Image
 												src={
-													'/images/icons/vouched.svg'
+													'/images/icons/trash-black.svg'
 												}
-												alt={'vouched'}
-												width={24}
-												height={24}
-											/>
-											Vouched
-										</span>
-									) : (
-										<span className='gap-4 flex items-center'>
-											<Image
-												src={
-													'/images/icons/red-flag.svg'
-												}
-												alt={'red-flag'}
+												alt={'edit'}
 												width={18}
 												height={18}
 											/>
-											Flagged
-										</span>
-									)}
-								</div>
-								<div className='flex flex-row px-4 py-6 align-top text-gray-800'>
-									<button className='flex flex-row mr-2 border border-gray text-black font-bold px-4 py-2 gap-2 items-center'>
-										Edit{' '}
-										<Image
-											src={'/images/icons/edit.svg'}
-											alt={'edit'}
-											width={16}
-											height={16}
-										/>
-									</button>
-									<button className='mr-2 border border-gray text-black font-bold px-4 py-2'>
-										<Image
-											src={
-												'/images/icons/trash-black.svg'
-											}
-											alt={'edit'}
-											width={18}
-											height={18}
-										/>
-									</button>
-								</div>
-							</div>
-						))}
+										</button>
+									</div>
+								</React.Fragment>
+							))}
+						</div>
 					</div>
 				)}
 			</div>
