4fb2c04d391d394b414cf081cc744a6d3156e0cd
Cherik <Pourcheriki@gmail.com>
2024-06-06 17:53:52 +0330

    fix title and desc

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -169,10 +169,10 @@ export const ProjectCard: FC<IProjectCardProps> = ({ project, queryKey }) => {
 						)}
 					</div>
 					<div className='flex-1'>
-						<h3 className='text-2xl font-bold mb-2'>
+						<h3 className='text-2xl font-bold mb-2  max-w-full break-all'>
 							{project.title || NO_DATA}
 						</h3>
-						<p className='text-gray-400'>
+						<p className='text-gray-400 max-w-full break-all'>
 							{project.description
 								? project.description.length >
 									PROJECT_DESC_LIMIT
