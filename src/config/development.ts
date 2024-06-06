118d7e9e158766b3570938c4a8e91f652f064065
Cherik <Pourcheriki@gmail.com>
2024-06-06 18:35:41 +0330

    move PROJECT_VERIFY_SCHEMA to config

--- src/config/development.ts
+++ src/config/development.ts
@@ -5,9 +5,6 @@ const config = {
 	EAS_CONTRACT_ADDRESS:
 		process.env.NEXT_PUBLIC_EAS_CONTRACT_ADDRESS ||
 		'0xC2679fBD37d54388Ce493F1DB75320D236e1815e',
-	PROJECT_VERIFY_SCHEMA:
-		process.env.NEXT_PUBLIC_PROJECT_VERIFY_SCHEMA ||
-		'0x97b0c9911936fad57e77857fac6eef6771f8d0bf025be9549214e32bf9e2415a',
 	SOURCE_PLATFORMS: [
 		{ key: 'Giveth', value: 'giveth' },
 		{ key: 'Gitcoin', value: 'gitcoin' },
