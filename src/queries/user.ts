8a3e948b2a9162c4450965c1b904a6e1ba421354
Amin Latifi <a.latifi.al@gmail.com>
2024-06-06 18:17:28 +0330

    Fixed usr org repeat

--- src/queries/user.ts
+++ src/queries/user.ts
@@ -1,6 +1,6 @@
 export const FETCH_USER_ORGANISATIONS = `
 	query attestorOrganisations($address: String) {
-		attestorOrganisations(where: {attestor: {id_eq: $address}}) {
+		attestorOrganisations(where: {attestor: {id_eq: $address}}, orderBy: attestTimestamp_ASC) {
 			id
 			organisation {
 				name
