dc587036f6a274a956bf4c9830376aa9d767ebb3
Cherik <Pourcheriki@gmail.com>
2024-06-06 18:05:35 +0330

    fix titles

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -169,7 +169,7 @@ export const ProjectCard: FC<IProjectCardProps> = ({ project, queryKey }) => {
 						)}
 					</div>
 					<div className='flex-1'>
-						<h3 className='text-2xl font-bold mb-2  max-w-full break-all'>
+						<h3 className='text-2xl font-bold mb-2  max-w-full break-word'>
 							{project.title || NO_DATA}
 						</h3>
 						<p className='text-gray-400 max-w-full break-all'>
