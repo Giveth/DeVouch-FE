6bc515b66b5f90451ef00186406507688a928249
Cherik <Pourcheriki@gmail.com>
2024-06-10 16:53:20 +0330

    fix project card link

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,18 +1,18 @@
 import { useRef, useState, useMemo, useCallback, type FC } from 'react';
 import Image from 'next/image';
-import Link from 'next/link';
 import { useAccount } from 'wagmi';
 import { useWeb3Modal } from '@web3modal/wagmi/react';
 import { Address } from 'viem';
 import { useQueryClient } from '@tanstack/react-query';
+import Link from 'next/link';
 import { AttestInfo } from './AttestsInfo/AttestInfo';
 import { OutlineButtonType, OutlineButton } from '../Button/OutlineButton';
 import { AttestModal } from '../Modal/AttestModal.tsx/AttestModal';
 import { type IProject, type ProjectAttestation } from '@/features/home/types';
 import { SourceBadge } from '../SourceBadge';
-import { ROUTES } from '@/config/routes';
 import { PROJECT_DESC_LIMIT } from './constant';
 import { AttestsInfo } from './AttestsInfo/AttestsInfo';
+import { ROUTES } from '@/config/routes';
 
 interface IProjectCardProps {
 	project: IProject;
@@ -129,10 +129,10 @@ export const ProjectCard: FC<IProjectCardProps> = ({ project, queryKey }) => {
 	return (
 		<div className='relative group/card max-w-full'>
 			<div className='absolute w-full h-full top-0 left-0 group-hover/card:top-2 group-hover/card:-left-2 bg-black transition-all '></div>
-			<Link
-				href={`${ROUTES.PROJECT}/${project.source}/${project.projectId}`}
-			>
-				<div className='p-8 border h-full border-gray-100 bg-white hover:border-black flex flex-col gap-6 relative'>
+			<div className='p-8 border h-full border-gray-100 bg-white hover:border-black flex flex-col gap-6 relative'>
+				<Link
+					href={`${ROUTES.PROJECT}/${project.source}/${project.projectId}`}
+				>
 					<div className='h-56 bg-blue-100 relative'>
 						{project.image && (
 							<Image
@@ -169,6 +169,10 @@ export const ProjectCard: FC<IProjectCardProps> = ({ project, queryKey }) => {
 							</div>
 						)}
 					</div>
+				</Link>
+				<Link
+					href={`${ROUTES.PROJECT}/${project.source}/${project.projectId}`}
+				>
 					<div className='flex-1'>
 						<h3 className='text-2xl font-bold mb-2  max-w-full break-word'>
 							{project.title || NO_DATA}
@@ -185,43 +189,44 @@ export const ProjectCard: FC<IProjectCardProps> = ({ project, queryKey }) => {
 								: NO_DATA}
 						</p>
 					</div>
-					<div>
-						<h4 className='text-lg font-bold mb-4'>Vouched By</h4>
-						<div className='flex gap-2 flex-wrap'>
-							<AttestsInfo attests={vouches} vouch />
-						</div>
-					</div>
-					<div>
-						<h4 className='text-lg font-bold mb-4'>Flagged By</h4>
-						<div className='flex gap-2 flex-wrap'>
-							<AttestsInfo attests={flags} />
-						</div>
-					</div>
-					<div className='flex flex-col md:flex-row gap-6'>
-						<OutlineButton
-							buttonType={OutlineButtonType.BLUE}
-							className='flex-1'
-							onClick={e => {
-								e.stopPropagation();
-								e.preventDefault();
-								onAttestClick(true);
-							}}
-						>
-							Vouch For Project
-						</OutlineButton>
-						<OutlineButton
-							buttonType={OutlineButtonType.RED}
-							onClick={e => {
-								e.stopPropagation();
-								e.preventDefault();
-								onAttestClick(false);
-							}}
-						>
-							Flag Project
-						</OutlineButton>
+				</Link>
+				<div>
+					<h4 className='text-lg font-bold mb-4'>Vouched By</h4>
+					<div className='flex gap-2 flex-wrap'>
+						<AttestsInfo attests={vouches} vouch />
 					</div>
 				</div>
-			</Link>
+				<div>
+					<h4 className='text-lg font-bold mb-4'>Flagged By</h4>
+					<div className='flex gap-2 flex-wrap'>
+						<AttestsInfo attests={flags} />
+					</div>
+				</div>
+				<div className='flex flex-col md:flex-row gap-6'>
+					<OutlineButton
+						buttonType={OutlineButtonType.BLUE}
+						className='flex-1'
+						onClick={e => {
+							e.stopPropagation();
+							e.preventDefault();
+							onAttestClick(true);
+						}}
+					>
+						Vouch For Project
+					</OutlineButton>
+					<OutlineButton
+						buttonType={OutlineButtonType.RED}
+						onClick={e => {
+							e.stopPropagation();
+							e.preventDefault();
+							onAttestClick(false);
+						}}
+					>
+						Flag Project
+					</OutlineButton>
+				</div>
+			</div>
+
 			{showAttestModal && (
 				<AttestModal
 					setShowModal={setShowAttestModal}
