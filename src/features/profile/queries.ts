ba2b32ac131d596b604bc64100b50258f485e355
Cherik <Pourcheriki@gmail.com>
2024-06-12 12:17:32 +0330

    add source to query

--- src/features/profile/queries.ts
+++ src/features/profile/queries.ts
@@ -23,14 +23,14 @@ query fetchUserAttestations($address: String, $vouch: Boolean, $organisation: [S
 `;
 
 export const FETCH_USER_ATTESTATIONS_TOTAL_COUNT = `
-query fetchUserAttestations($address: String, $organisation: [String!]) {
-  vouches: projectAttestationsConnection(first: 0, orderBy: id_ASC, where: {attestorOrganisation: {attestor: {id_eq: $address}, organisation: {id_in: $organisation}}, AND: {vouch_eq: true}}) {
+query fetchUserAttestations($address: String, $organisation: [String!], $source_in: [String!]) {
+  vouches: projectAttestationsConnection(first: 0, orderBy: id_ASC, where: {attestorOrganisation: {attestor: {id_eq: $address}, organisation: {id_in: $organisation}}, AND: {vouch_eq: true, project: {source_in: $source_in}}}) {
     totalCount
   }
-  flags: projectAttestationsConnection(first: 5, orderBy: id_ASC, where: {attestorOrganisation: {attestor: {id_eq: $address}, organisation: {id_in: $organisation}}, AND: {vouch_eq: false}}) {
+  flags: projectAttestationsConnection(first: 5, orderBy: id_ASC, where: {attestorOrganisation: {attestor: {id_eq: $address}, organisation: {id_in: $organisation}}, AND: {vouch_eq: false, project: {source_in: $source_in}}}) {
     totalCount
   }
-  attests: projectAttestationsConnection(first: 5, orderBy: id_ASC, where: {attestorOrganisation: {attestor: {id_eq: $address}, organisation: {id_in: $organisation}}}) {
+  attests: projectAttestationsConnection(first: 5, orderBy: id_ASC, where: {attestorOrganisation: {attestor: {id_eq: $address}, organisation: {id_in: $organisation}}, AND: {project: {source_in: $source_in}}}) {
     totalCount
   }
 }
