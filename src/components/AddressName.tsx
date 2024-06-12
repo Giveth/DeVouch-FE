92adc850f163f3fd84c2ce08a3f93470a78974c1
Cherik <Pourcheriki@gmail.com>
2024-06-12 14:19:51 +0330

    update AddressName

--- src/components/AddressName.tsx
+++ src/components/AddressName.tsx
@@ -1,14 +1,34 @@
-import { type FC } from 'react';
-import { Address } from 'viem';
-import { useEnsName } from 'wagmi';
+import { useEffect, useState, type FC } from 'react';
+import { Address, createPublicClient, http } from 'viem';
+import { mainnet } from 'viem/chains';
 import { summarizeAddress } from '@/helpers/wallet';
 
+const publicClient = createPublicClient({
+	chain: mainnet,
+	transport: http(),
+});
 interface AddressNameProps {
 	address?: Address;
 }
 
 export const AddressName: FC<AddressNameProps> = ({ address }) => {
-	const { data: ensName } = useEnsName({ address, chainId: 1 });
+	const [ensName, setEnsName] = useState('');
+	useEffect(() => {
+		if (!address) return;
+		const _getEnsName = async (address: Address) => {
+			try {
+				const ensName = await publicClient.getEnsName({
+					address,
+				});
+				if (!ensName) return;
+				setEnsName(ensName);
+			} catch (e) {
+				console.log({ e });
+				return null;
+			}
+		};
+		_getEnsName(address);
+	}, [address]);
 
 	return <span>{ensName || summarizeAddress(address)}</span>;
 };
