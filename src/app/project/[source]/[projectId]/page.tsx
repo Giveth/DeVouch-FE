66ab60df919f1bb104d9cfbf03c3a1d77692d816
Cherik <Pourcheriki@gmail.com>
2024-06-16 16:01:04 +0330

    add title and image

--- src/app/project/[source]/[projectId]/page.tsx
+++ src/app/project/[source]/[projectId]/page.tsx
@@ -1,9 +1,31 @@
 import { Suspense } from 'react';
+import { Metadata, ResolvingMetadata } from 'next';
 import {
 	ProjectDetails,
 	ProjectDetailsProps,
 } from '@/features/project/ProjectDetails';
 import { Spinner } from '@/components/Loading/Spinner';
+import { fetchProjectData } from '@/features/project/services';
+
+type Props = {
+	params: { source: string; projectId: string };
+};
+
+export async function generateMetadata(
+	{ params }: Props,
+	parent: ResolvingMetadata,
+): Promise<Metadata> {
+	const { source, projectId } = params;
+	const project = await fetchProjectData(source, projectId);
+	const previousImages = (await parent).openGraph?.images || [];
+
+	return {
+		title: `DeVouch | ${project.title} Details`,
+		openGraph: {
+			images: [project.image, ...previousImages],
+		},
+	};
+}
 
 export default function Page({
 	params: { source, projectId },
