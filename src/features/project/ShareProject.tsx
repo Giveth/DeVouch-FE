117765551c858cb88d952a0189f7d0b9f316fbcf
Cherik <Pourcheriki@gmail.com>
2024-06-19 17:22:32 +0330

    add copied state

--- src/features/project/ShareProject.tsx
+++ src/features/project/ShareProject.tsx
@@ -1,10 +1,15 @@
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 
 export const ShareProject = () => {
+	const [isCopied, setIsCopied] = useState(false);
 	const copyToClipboard = () => {
+		setIsCopied(true);
 		const address = window.location.href;
 		navigator.clipboard.writeText(address);
+		setTimeout(() => {
+			setIsCopied(false);
+		}, 2000);
 	};
 	return (
 		<div className='flex flex-col md:flex-row py-6 justify-between items-center text-center gap-2 mt-[-20px]'>
@@ -12,17 +17,24 @@ export const ShareProject = () => {
 				share this project with your community and invite them to
 				attest!
 			</span>
-			<button
-				onClick={copyToClipboard}
-				className='bg-white w-full md:w-auto border border-gray-300 text-gray-500 gap-2 px-4 py-2 '
-			>
-				<Image
-					src={'/images/icons/share.svg'}
-					alt={'share'}
-					width={18}
-					height={18}
-				/>
-			</button>
+			<div className='flex items-center'>
+				<button
+					onClick={copyToClipboard}
+					className='bg-white min-w-fit w-full min-h-11 md:w-auto border border-gray-300 text-black gap-2 px-4 py-2 transition-all duration-1000 ease-in-out'
+				>
+					{isCopied ? (
+						<span className='text-sm'>Copied!</span>
+					) : (
+						<Image
+							src={'/images/icons/share.svg'}
+							alt={'share'}
+							width={16}
+							height={16}
+							className='py-0'
+						/>
+					)}
+				</button>
+			</div>
 		</div>
 	);
 };
