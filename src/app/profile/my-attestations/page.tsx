4f78f8221a0371454789c9795a66fab2d64de6d1
Cherik <Pourcheriki@gmail.com>
2024-06-19 16:23:12 +0330

    add my attestation page

--- src/app/profile/my-attestations/page.tsx
+++ src/app/profile/my-attestations/page.tsx
@@ -1,4 +1,16 @@
+import { Metadata } from 'next';
 import { UserAttestations } from '@/features/profile/UserAttestations';
+
+export const metadata: Metadata = {
+	title: 'DeVouch | My Attestations',
+	description: 'View your flagged or vouched projects using DeVouch.',
+	openGraph: {
+		title: 'DeVouch | My Attestations',
+		description: 'View your flagged or vouched projects using DeVouch.',
+		type: 'website',
+		images: ['https://devouch.app/images/banner.png'],
+	},
+};
 
 export default function UserAttestationsPage() {
 	return <UserAttestations />;
