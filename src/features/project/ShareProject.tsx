eb4361a7ec34c50732f828dbb15dcae8461ceb0d
Cherik <Pourcheriki@gmail.com>
2024-06-19 16:50:54 +0330

    copy address

--- src/features/project/ShareProject.tsx
+++ src/features/project/ShareProject.tsx
@@ -2,13 +2,20 @@ import Image from 'next/image';
 import React from 'react';
 
 export const ShareProject = () => {
+	const copyToClipboard = () => {
+		const address = window.location.href;
+		navigator.clipboard.writeText(address);
+	};
 	return (
 		<div className='flex flex-col md:flex-row py-6 justify-between items-center text-center gap-2 mt-[-20px]'>
 			<span className='block text-gray-500 capitalize'>
 				share this project with your community and invite them to
 				attest!
 			</span>
-			<button className='bg-white w-full md:w-auto border border-gray-300 text-gray-500 gap-2 px-4 py-2 flex items-center justify-center'>
+			<button
+				onClick={copyToClipboard}
+				className='bg-white w-full md:w-auto border border-gray-300 text-gray-500 gap-2 px-4 py-2 '
+			>
 				<Image
 					src={'/images/icons/share.svg'}
 					alt={'share'}
