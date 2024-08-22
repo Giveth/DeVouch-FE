abdef7f8cf3765da5c0d25d765468f6f5b97b4ae
Cherik <Pourcheriki@gmail.com>
2024-08-22 12:16:48 +0330

    handle rf source

--- src/components/SourceBadge.tsx
+++ src/components/SourceBadge.tsx
@@ -1,29 +1,38 @@
 import Image from 'next/image';
 import { type FC, type HTMLAttributes } from 'react';
 import config from '@/config/configuration';
+import { IProject } from '@/features/home/types';
 
 interface SourceBadgeProps {
-	source?: string;
+	source?: IProject['source'];
+	rfRound?: IProject['rfRounds'];
 	className?: HTMLAttributes<HTMLDivElement>['className'];
 }
 
-export const SourceBadge: FC<SourceBadgeProps> = ({ source, className }) => {
+export const SourceBadge: FC<SourceBadgeProps> = ({
+	source,
+	rfRound,
+	className = '',
+}) => {
+	const roundSuffix = rfRound?.length ? `${rfRound.slice(-1)}` : '';
+	const formattedSource =
+		source === 'rf' ? `${source}${roundSuffix}` : source;
+	const platform = config.SOURCE_PLATFORMS.find(
+		i => i.value.toLowerCase() === formattedSource?.toLowerCase(),
+	);
+
 	return source ? (
-		<div className={` flex gap-1 bg-white py-1 px-2 z-auto ${className}`}>
-			<span className='text-gray-300 font-light'>From</span>
-			<span className='text-black'>
-				{
-					config.SOURCE_PLATFORMS.find(
-						i => i.value.toLowerCase() === source.toLowerCase(),
-					)?.key
-				}
-			</span>
-			<Image
-				src={`/images/sources/${source}.svg`}
-				alt={source}
-				width={18}
-				height={18}
-			/>
+		<div className={`flex gap-1 bg-white py-1 px-2 z-auto ${className}`}>
+			<span className='text-gray-300 font-light'>From </span>
+			<span className='text-black'>{platform?.key}</span>
+			{platform && (
+				<Image
+					src={`/images/sources/${formattedSource}.svg`}
+					alt={formattedSource || 'source icon'}
+					width={18}
+					height={18}
+				/>
+			)}
 		</div>
 	) : null;
 };
