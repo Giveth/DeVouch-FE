afa28d6f6b12be72ff28448b03868fd0ebe11c6f
Kilter <mateodaza@gmail.com>
2024-08-26 17:48:41 -0500

    refactoring query for multiple rf

--- src/features/home/query-genrator.ts
+++ src/features/home/query-genrator.ts
@@ -2,12 +2,29 @@ export const generateFetchProjectsQuery = (
 	projectSource?: string[],
 	organisationId?: string[],
 	term?: string,
+	rfRounds?: number[],
 ) => {
 	const conditions = ['imported_eq: true'];
 	const props = [];
+
 	if (projectSource && projectSource.length > 0) {
-		conditions.push('source_in: $project_source');
-		props.push('$project_source: [String!]');
+		const nonRfSources = projectSource.filter(
+			source => !source.startsWith('rf'),
+		);
+		const hasRfSource = projectSource.some(source =>
+			source.startsWith('rf'),
+		);
+
+		if (nonRfSources.length > 0) {
+			conditions.push('source_in: $non_rf_sources');
+			props.push('$non_rf_sources: [String!]');
+		}
+
+		if (hasRfSource && rfRounds && rfRounds.length > 0) {
+			conditions.push('source_eq: "rf"');
+			conditions.push('rfRounds_containsAny: $rf_rounds');
+			props.push('$rf_rounds: [Int!]');
+		}
 	}
 
 	if (organisationId && organisationId.length > 0) {
@@ -26,42 +43,43 @@ export const generateFetchProjectsQuery = (
 
 	const addedProps = props.length > 0 ? `,${props.join(', ')}` : '';
 
-	return `
+	const query = `
 	query fetchProjects(
-		$offset: Int!,
-		$limit: Int,
-		$orderBy: [ProjectOrderByInput!] = lastUpdatedTimestamp_DESC
-		${addedProps}
+	  $offset: Int!,
+	  $limit: Int,
+	  $orderBy: [ProjectOrderByInput!] = lastUpdatedTimestamp_DESC
+	  ${addedProps}
+	) {
+	  projects(
+		offset: $offset,
+		limit: $limit,
+		orderBy: $orderBy,
+		${whereClause}
 	  ) {
-		projects(
-		  offset: $offset,
-		  limit: $limit,
-		  orderBy: $orderBy,
-		  ${whereClause}
-		) {
+		id
+		projectId
+		title
+		descriptionSummary
+		image
+		source
+		rfRounds
+		url
+		attests {
 		  id
-		  projectId
-		  title
-		  descriptionSummary
-		  image
-		  source
-		  rfRounds
-		  url
-		  attests {
-			id
-			vouch
-			attestorOrganisation {
-			  attestor {
-				id
-			  }
-			  organisation {
-				id
-				name
-				color
-			  }
+		  vouch
+		  attestorOrganisation {
+			attestor {
+			  id
+			}
+			organisation {
+			  id
+			  name
+			  color
 			}
 		  }
 		}
 	  }
-`;
+	}
+  `;
+	return query;
 };
