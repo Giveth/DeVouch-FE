0fa50359bc046bd9aac49b87c9a60a16fe6d78e8
Mitch Oz <divine_comedian@protonmail.com>
2025-01-29 15:15:14 -0600

    update documentation links

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,6 +2,6 @@ export const DEFAULT_ORGANISATION_COLOR = '#ee3cbf';
 export const links = {
 	GIVETH_WEBSITE: 'https://giveth.io',
 	DISCORD_LINK: 'https://discord.giveth.io',
-	DOCUMENTATION_LINK: 'https://docs.giveth.io/devouch/',
+	DOCUMENTATION_LINK: 'https://docs.giveth.io/devouch/what-is-devouch',
 	INTEGRATE_LINK: 'https://docs.giveth.io/devouch/integrating-devouch',
 };
